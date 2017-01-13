ee07b6fb4275597cc4ddefe65db4f3488f33d0ad
Joram Kimata <joramkimata@gmail.com>
2017-01-13 11:46:43 +0300

    added authority package

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'mspro',
+			'username'  => 'root',
+			'password'  => 'Kimata#9012',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
